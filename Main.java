a43a185308e0e207454c3104bf4ef710d00300d9
Dylan <dylanhurtos@gmail.com>
2019-10-08 16:22:47 +0200

    Primer commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29326.143
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {C9E6E2EF-286D-4B10-B0DE-5E614A2DF42B}
+	EndGlobalSection
+EndGlobal
